2a1590e572544ab6b6f663386dc49d1beb1cf185
Karanbir Chahal <karanbleep@gmail.com>
2016-04-09 16:28:22 +0530

    Models for rec. engine

--- models/account.js
+++ models/account.js
@@ -7,6 +7,59 @@ var Account = new Schema({
     password: String
 });
 
+var Male = new Schema({
+    age_group: {
+      min:Number,
+      max:Number,
+      tags:[{
+        name:String,
+        number:Number
+      }]
+    },
+    season: {
+      type:String, // winter,summer,spring,fall
+      tags:[{
+        name:String,
+        number:Number
+      }]
+    },
+    occupation: {
+      type: String, // student,professional,buisness,retired
+      tags:[{
+        name:String,
+        number:Number
+      }]
+    }
+
+});
+
+var Female = new Schema({
+
+    age_group: {
+      min:Number,
+      max:Number,
+      tags:[{
+        name:String,
+        number:Number
+      }]
+    },
+    season: {
+      type:String, // winter,summer,spring,fall
+      tags:[{
+        name:String,
+        number:Number
+      }]
+    },
+    occupation: {
+      type: String, // student,professional,buisness,retired
+      tags:[{
+        name:String,
+        number:Number
+      }]
+    }
+
+});
+
 Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Account', Account);
